d9f0e40204e806a82cfcba1210b26c3080714c15
sebam2k4 <sebam2k4@gmail.com>
2018-05-03 23:32:18 +0100

    complete hard challenge

--- theforce.js
+++ theforce.js
@@ -1,8 +1,8 @@
 document.addEventListener("DOMContentLoaded", function(){
 
-/*********************
-*** EASY CHALLENGE ***
-*********************/
+  /*********************
+  *** EASY CHALLENGE ***
+  *********************/
 
   // get html collection of option elements
   let options = document.getElementById('jedi-list').options;
@@ -15,5 +15,37 @@ document.addEventListener("DOMContentLoaded", function(){
     options[i].classList.add(classes[i]);
   }
 
-});
+  
+  /*********************
+  *** HARD CHALLENGE ***
+  *********************/
 
+  // create ids for elements with 'checkboxHolder' class
+  // this will make it possible to style each checkbox individually in css
+  let checkboxHolders = document.querySelectorAll(".checkboxholder");
+  for (let i = 0; i < checkboxHolders.length; i++) {
+    checkboxHolders[i].id = `checkboxHolder-${i}`;
+  }
+
+  // ADD SOUND TO LIGHTSABERS
+  // get the checkbox
+  const lightsabers = document.querySelectorAll('.checkboxholder input[type="checkbox"]');
+  
+  // add sound to each hard checkbox
+  for (let i = 0; i < lightsabers.length; i++) {
+    lightsaber = lightsabers[i]
+    lightsaber.addEventListener('change', playLightsaberSound);
+  }
+
+  // play sound when turning checkbox on
+  function playLightsaberSound(e) {
+    console.log(e.target.checked)
+    const checked = e.target.checked;
+    if (checked) {
+      const lightsaberSound = document.createElement('audio');
+      lightsaberSound.src = 'lightsaber.wav';
+      lightsaberSound.play();
+    }
+  }
+
+});
